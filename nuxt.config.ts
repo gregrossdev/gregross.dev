52480b627d768f0470e30146a6a39257bba4721c
Gregory Ross <gregross.dev@gmail.com>
2023-10-19 22:59:08 -0400

    views animation transition with pinia store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-      '@nuxt/content',
-      '@nuxt/ui'
-  ],
+  modules: ['@nuxt/content', '@nuxt/ui', '@pinia/nuxt'],
   content: {
     // ... options
-  }
+  },
+  image: {
+    // Options
+  },
+  experimental: {
+    jsTsFormatResource: true,
+    viewTransition: true,
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <script lang="ts" setup>
+import { useBlogStore} from "../stores/blog";
+const blogStore = useBlogStore();
+
 useHead({
   title: "Nuxt Content Blog Demo",
 });
+
 </script>
 
 <template>
@@ -25,20 +29,21 @@ useHead({
           v-for="blog in list"
           :key="blog._path"
           class="blog-card bg-white rounded-2xl overflow-hidden mb-4"
+          :class="{active: blogStore.activeArticle && article._id === blogStore.activeArticle._id}"
       >
         <figure class="h-[300px] relative">
           <img
               v-if="blog.thumbnail"
               :src="blog.thumbnail"
               :alt="blog.title"
-              class="absolute w-full h-full object-cover"
+              class="blog-thumbnail absolute w-full h-full object-cover"
           />
         </figure>
 
         <div class="blog-card--meta my-4 ml-4">
-          <h3 class="text-2xl font-bold">
-            <NuxtLink :to="blog.slug">{{ blog.title }}</NuxtLink>
-          </h3>
+          <h2 class="blog-title text-2xl font-bold">
+            <NuxtLink :to="blog.slug" @click="blogStore.setActiveArticle(article)">{{ blog.title }}</NuxtLink>
+          </h2>
           <div class="text-sm text-gray-500 mt-px block">{{ blog.date }}</div>
           <div v-if="blog.tags" class="mt-2 text-xs">
             <span v-for="tag in blog.tags" class="p-1 rounded bg-gray-100 mr-2">
@@ -53,4 +58,12 @@ useHead({
   </div>
 </template>
 
-<style></style>
+<style>
+  .blog-thumbnail {
+    view-transition-name: blog-thumbnail;
+  }
+
+  .blog-title {
+    view-transition-name: blog-title;
+  }
+</style>
